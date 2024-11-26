aaa4e69a2103480e7d8507b4bc26c1079c3f3569
minsk0105 <cminseog7065@gmail.com>
2024-11-26 17:18:23 +0900

    .

---
+++ Web_Technician/Gangwon/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    var i = 4;
+
+    setInterval(function() {
+        i--;
+
+        // if (i === 3) $("article img").css("right", "0");
+        // $("article img").eq(i).animate({"right": "1200px"}, 500);
+
+        if (i === 3) $("article img").css("display", "block");
+        $("article img").eq(i).fadeOut(500);
+
+        if (i <= 1) i = 4;
+    }, 2000);
+
+    $(".menu").hover(function() {
+        $(".submenu").stop().slideDown();
+    }, function() {
+        $(".submenu").stop().slideUp();
+    });
+
+});
