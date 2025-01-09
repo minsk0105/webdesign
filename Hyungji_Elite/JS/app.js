f553cd8fc1ae47c2f43809f041c1cf378d55f346
minsk0105 <cminseog7065@gmail.com>
2025-01-09 20:33:28 +0900

    .

--- Hyungji_Elite/JS/app.js
+++ Hyungji_Elite/JS/app.js
@@ -3,15 +3,22 @@ const langList = document.querySelector('.lang_list');
 const shareBtn = document.querySelector('.share');
 const shareMenu = document.querySelector('.share_menu');
 
-const toggleShow = (btn, box) => {
+const toggleShow = (btn, box, value = "") => {
     btn.addEventListener('click', () => {
-        if (box.classList.contains("show_lang")) {
-            box.classList.remove("show_lang");
+        if (box.classList.contains(value)) {
+            box.classList.remove(value);
         } else {
-            box.classList.add("show_lang");
+            box.classList.add(value);
         }
     });
 };
 
-toggleShow(langBtn, langList);
-toggleShow(shareBtn, shareMenu);
+toggleShow(langBtn, langList, "show_lang");
+toggleShow(shareBtn, shareMenu, "show_lang");
+
+const siteBtns = document.querySelectorAll('.site_btn');
+const siteMap = document.querySelector('.site_map');
+
+siteBtns.forEach((btn) => {
+    toggleShow(btn, siteMap, "show_site");
+});
